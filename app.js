8eb351d3951d977e2cf62833594bcbbc9b853a17
Death301 <aaditya.uzumaki@gmail.com>
2024-04-17 02:23:57 +0530

    s

--- app.js
+++ app.js
@@ -6,24 +6,14 @@ const PORT = process.env.PORT || 3000;
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
-<<<<<<< HEAD
 // Optional: Define routes for specific HTML files if needed
 // Route for the Kanban board
 
 // Example route for the home page
-=======
-// Route for the login page
->>>>>>> 000deb455bcb0ae34c3267258cb2aa21cab534a7
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
-<<<<<<< HEAD
-=======
-// Additional routes for other specific HTML files if needed
-
-// Route for the Kanban board page
->>>>>>> 000deb455bcb0ae34c3267258cb2aa21cab534a7
 app.get('/kanban', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'kanban.html'));
 });
@@ -32,15 +22,12 @@ app.get('/kanban', (req, res) => {
 app.get('/eisenhower', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'eisenhower.html'));
 });
-<<<<<<< HEAD
 //login
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 // Handling admin panel assets
 app.use('/admin', express.static(path.join(__dirname, 'public', 'Admin')));
-=======
->>>>>>> 000deb455bcb0ae34c3267258cb2aa21cab534a7
 
 // Start the server
 app.listen(PORT, () => {
